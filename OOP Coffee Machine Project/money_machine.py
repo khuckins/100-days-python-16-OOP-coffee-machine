86c9dd384bb41a5bc3cd13f7afbe75694ac671fb
Kyle Huckins <kylehenglish@gmail.com>
2025-07-16 13:47:53 -0700

    Coin Request Type-Safe + Cancel Order
    - Make coin request type-safe
    - Allow cancelling an order at the payment screen
    
    Signed-off-by: Kyle Huckins <kylehenglish@gmail.com>

--- OOP Coffee Machine Project/money_machine.py
+++ OOP Coffee Machine Project/money_machine.py
@@ -21,7 +21,19 @@ class MoneyMachine:
         """Returns the total calculated from coins inserted."""
         print("Please insert coins.")
         for coin in self.COIN_VALUES:
-            self.money_received += int(input(f"How many {coin}?: ")) * self.COIN_VALUES[coin]
+            res = input(f"How many {coin}?: ").lower()
+            coin_inserted = 0
+            if res == 'cancel':
+                return coin_inserted
+
+            while True:
+                try:
+                    coin_inserted = int(res)
+                    break
+                except ValueError:
+                    print("Input must be a number. Input 'cancel' to cancel your order.")
+                    res = input(f"How many {coin}?: ")
+            self.money_received += coin_inserted * self.COIN_VALUES[coin]
             if self.money_received >= cost:
                 break
         return self.money_received
@@ -36,6 +48,6 @@ class MoneyMachine:
             self.money_received = 0
             return True
         else:
-            print("Sorry that's not enough money. Money refunded.")
+            print("Not enough money provided. Money refunded.")
             self.money_received = 0
             return False
